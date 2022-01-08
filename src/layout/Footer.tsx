b11f67ad7a692f9a0743005753238362ae50df30
yoshiaki44 <yo44ch@gmail.com>
2022-01-08 23:52:40 +0900

    css変更

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,11 +1,11 @@
 import type { VFC } from "react";
-
 /**
  * @package
  */
+
 export const Footer: VFC = () => {
   return (
-    <footer>
+    <footer className="py-5 px-3 text-gray-600 bg-white">
       <small>&copy; 20xx example</small>
     </footer>
   );
