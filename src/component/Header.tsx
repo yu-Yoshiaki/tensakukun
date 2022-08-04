7a9f2ca7d611f0bf84bf308b1cd7441d7350ed69
yoshiaki <yoshiosmile68@gmail.com>
2022-08-04 14:08:24 +0900

    kaizen

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,8 +1,7 @@
-import { HomeIcon } from "@heroicons/react/outline";
 import { NavLink } from "src/component/Button";
 
 const items = [
-  { href: "/", label: "HOME", icon: <HomeIcon className="w-6 h-7" /> },
+  { href: "/", label: "HOME" },
   { href: "/about", label: "ABOUT" },
 ];
 
@@ -14,12 +13,11 @@ export const Header = () => {
     <header className="flex justify-between items-center p-4 bg-white">
       <h1 className="text-4xl font-bold text-center">Title</h1>
       <nav className="flex">
-        {items.map(({ href, label, icon }) => {
+        {items.map(({ href, label }) => {
           return (
             <NavLink key={href} href={href} activeClassName="text-blue-500 ">
               <a className="flex justify-between items-center py-4 px-6 text-lg hover:text-white hover:bg-black">
-                <p>{icon ?? ""}</p>
-                <p>{label}</p>
+                {label}
               </a>
             </NavLink>
           );
