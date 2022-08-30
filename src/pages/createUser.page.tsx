c521bed765736b66eda3a6df8b133ec4dc103ee0
yumotoyoshiaki <yoshiosmile68@gmail.com>
2022-08-30 11:21:04 +0900

    kaizen

---
+++ src/pages/createUser.page.tsx
@@ -0,0 +1,11 @@
+import { CreateUserForm } from "src/component/CreateUserForm";
+
+const createUser = () => {
+  return (
+    <div>
+      <CreateUserForm />
+    </div>
+  );
+};
+
+export default createUser;
