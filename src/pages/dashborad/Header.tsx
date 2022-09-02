46d50fe2439bba31edb635397ac29f6bfb796f5b
yumotoyoshiaki <yoshiosmile68@gmail.com>
2022-09-02 17:56:37 +0900

    変更

--- src/pages/dashborad/Header.tsx
+++ src/pages/dashborad/Header.tsx
@@ -2,7 +2,7 @@ export const Header = () => {
   return (
     <header className="flex justify-between items-center px-3 bg-green-400 rounded-xl">
       <h1 className="flex items-center px-5 h-[80px] text-2xl font-bold">
-        CHU-HOTEL-ONLINE
+        HOTEL-TOKYO-OWNER
       </h1>
       <button className="px-5 h-[60px] bg-yellow-200 rounded-xl">
         ログイン
