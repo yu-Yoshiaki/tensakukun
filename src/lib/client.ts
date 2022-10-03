2e81fd1fe442b14fec603a3a3b1115a011259288
yumotoyoshiaki <yoshiosmile68@gmail.com>
2022-10-03 16:54:09 +0900

    fix

--- src/lib/client.ts
+++
@@ -1,8 +0,0 @@
-const line = require("@line/bot-sdk");
-
-import type { Client } from "@line/bot-sdk";
-
-export const client: Client = new line.Client({
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN as string,
-  channelSecret: process.env.CHANNEL_SECRET as string,
-});
