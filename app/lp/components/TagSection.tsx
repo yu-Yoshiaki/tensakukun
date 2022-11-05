459cf1b21281a024227a98fd49d0c1d0b00b5046
yumotoyoshiaki <yoshiosmile68@gmail.com>
2022-11-05 17:22:33 +0900

    test

--- app/lp/components/TagSection.tsx
+++ app/lp/components/TagSection.tsx
@@ -1,11 +1,10 @@
 import { useWindowSize } from "app/hooks/useWindowSize";
 import { Center } from "app/lp/components/Center";
 import { Section } from "app/lp/components/Section";
-import YouTube from "react-youtube";
 
 export const TagSection = () => {
   const { windowSize, responsive } = useWindowSize();
-  const youtubeSize = () => {
+  const size = () => {
     if (windowSize.width > responsive.md) {
       return {
         height: "250",
@@ -19,23 +18,23 @@ export const TagSection = () => {
     }
   };
 
-  const opts = {
-    ...youtubeSize(),
-    playerVars: {
-      autoplay: 1,
-    },
-  };
-
   return (
     <Section title="タグ機能で詳細な顧客管理" id="tag">
       <Center>
-        {console.log("--------", opts)}
         <div className="md:grid grid-cols-2 gap-8">
           <p className="md:text-xl flex items-center">
             リンククリック、リッチメニュー、メッセージ配信など。お客様のアクション毎に多彩なタグを付与できます。
             <br /> 付与したタグをもとに次に行うべきアクションが可視化します。
           </p>
-          <YouTube videoId="zCoZ4VkJ-gg" opts={opts} />
+          <iframe
+            width={size().width}
+            height={size().height}
+            src="https://www.youtube.com/embed/zCoZ4VkJ-gg"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
         </div>
       </Center>
     </Section>
